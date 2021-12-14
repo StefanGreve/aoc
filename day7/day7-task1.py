95fb00548dfe3cbe0a61f0e2814421727fc1b779
StefanGreve <greve.stefan@outlook.jp>
2021-12-15 00:40:14 +0100

    Solve day 7 in Python

---
+++ day7/day7-task1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+def read_file(path: str) -> List[int]:
+    with open(path, mode='r', encoding='utf-8') as file_handler:
+        return list(map(int, file_handler.readline().split(',')))
+
+def main(path: str) -> int:
+    fuels = read_file(path)
+    cost = lambda k: sum(map(lambda x: abs(x-k), fuels))
+    return min(map(cost, range(0, max(fuels))))
+
+if __name__ == '__main__':
+    result = main('input')
+    print(f"The horizontal position that the crabs can align to using the least fuel possible is {result}.")
