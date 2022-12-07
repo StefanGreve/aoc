ec307dbceed0a27cb1a3b4b64f51ee5d10280c07
StefanGreve <greve.stefan@outlook.jp>
2022-12-07 22:45:48 +0100

    Implement solution for day 5

--- 2022/AoC2022/Common.cs
+++ 2022/AoC2022/Common.cs
@@ -9,6 +9,31 @@ namespace AoC2022
 {
     public static class Common
     {
-        // TODO
+        public static IEnumerable<IEnumerable<T>> Transpose<T>(this IEnumerable<IList<T>> source)
+        {
+            var enumerators = source.Map(t => t.GetEnumerator()).Filter(e => e.MoveNext());
+
+            while (enumerators.Any())
+            {
+                yield return enumerators.Map(e => e.Current);
+                enumerators = enumerators.Filter(e => e.MoveNext());
+            }
+        }
+
+        public static IEnumerable<T> Pop<T>(this IList<T> source, int n = 1)
+        {
+            while (n > 0)
+            {
+                T value = source.Last();
+                source.RemoveAt(source.Count - 1);
+                yield return value;
+                n--;
+            }
+        }
+
+        public static void Push<T>(this IList<T> source, IEnumerable<T> values)
+        {
+            foreach (T value in values) source.Add(value);
+        }
     }
 }
--- 2022/Day5/Tasks.cs
+++ 2022/Day5/Tasks.cs
@@ -1,36 +1,66 @@
 ﻿using System.Linq;
+using System.Runtime.CompilerServices;
 
 using LanguageExt;
 
 namespace AoC2022.Day5
 {
+    public record Move
+    {
+        public int Amount { get; set; }
+
+        public int From { get; set; }
+
+        public int To { get; set; }
+    }
+
     public class Day5
     {
-        // 1st get crates as list
-        // 2nd parse instructions
-
-        public static IEnumerable<List<(int Min, int Max)>> ParseInput(string[] input)
+        public static List<List<char>> ParseCrates(string[] input)
         {
-            throw new NotImplementedException();
+            return input
+                .TakeWhile(s => s.Contains("["))
+                .Map(s => s.ToCharArray())
+                .Transpose()
+                .Filter(x => x.Any(char.IsLetter))
+                .Map(x => x.Filter(y => !char.IsWhiteSpace(y)).Reverse().ToList())
+                .ToList();
         }
 
-        public static int Task1(IEnumerable<List<(int Min, int Max)>> sequence)
+        public static IEnumerable<Move> ParseMoves(string[] input)
         {
-            throw new NotImplementedException();
+            return input
+                .SkipWhile(x => !x.StartsWith("move"))
+                .Map(x => x.Split(" ").Filter(y => y.All(char.IsDigit)))
+                .Map(x => x.Map(Convert.ToInt32).ToList())
+                .Select(x => new Move { Amount = x[0], From = x[1] - 1, To = x[2] - 1 });
         }
 
-        public static int Task2(IEnumerable<List<(int Min, int Max)>> sequence)
+        public static string Task1(string[] input, int crateMover)
         {
-            throw new NotImplementedException();
+            var crates = ParseCrates(input);
+            var moves = ParseMoves(input);
+
+            foreach (var move in moves)
+            {
+                var tmp = crates[move.From].Pop(move.Amount);
+                crates[move.To].Push(crateMover == 9000 ? tmp : tmp.Reverse());
+            }
+
+            return string.Join(string.Empty, crates.Map(x => x.Last()));
+        }
+
+        public static string Task2(string[] input)
+        {
+            return Task1(input, 9001);
         }
 
         public static void Main(string[] args)
         {
-            var input = File.ReadAllLines("input4.txt");
-            var data = ParseInput(input);
+            var input = File.ReadAllLines("input5.txt");
 
-            var result1 = Task1(data);
-            var result2 = Task2(data);
+            var result1 = Task1(input, crateMover: 9000);
+            var result2 = Task2(input);
 
             Console.WriteLine($"Task 1 = {result1}");
             Console.WriteLine($"Task 2 = {result2}");
